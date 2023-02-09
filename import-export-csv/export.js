22cd25660b7f62547400b054a14752fa66a948a6
johan <johan@majo.pw>
2023-02-09 22:13:40 +0100

    nths

--- import-export-csv/export.js
+++ import-export-csv/export.js
@@ -1,7 +1,10 @@
-const StoryblokClient = require('storyblok-js-client')
-const json2csv = require('json2csv')
-const fs = require('fs')
-const config = require('./config')
+// const StoryblokClient = require('storyblok-js-client')
+import StoryblokClient from 'storyblok-js-client'
+import fs from 'fs'
+// const fs = require('fs')
+// const config = require('./config')
+import config from './config.js'
+import { fileTypeFromBuffer } from 'file-type'
 
 // https://www.storyblok.com/docs/api/content-delivery#topics/authentication
 const Storyblok = new StoryblokClient({
@@ -13,19 +16,57 @@ const Storyblok = new StoryblokClient({
 })
 
 const start = async () => {
-  const response = await Storyblok.get('cdn/stories/', config.storyblok.export.options)
-  
-  const mapped = response.data.stories.map((story) => {
+  const response = await Storyblok.get('cdn/stories/home', {
+    version: 'draft',
+    resolve_relations: 'kavels'
+  }).catch((e) => console.log(e))
+
+
+  const kavels = response.data.rels
+  kavels.sort((a, b) => a.position - b.position)
+  kavels.reverse()
+
+  const mapped = kavels.map((story, index) => {
+
+    const afbeeldingen = []
+    story.content.afbeeldingen.forEach(afbeelding => {
+      const filename = afbeelding.filename.split('/').at(-1)
+
+
+      const download = async () => {
+        const response = await fetch(afbeelding.filename);
+        const arrayBuffer = await response.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+
+        const fileType = await fileTypeFromBuffer(buffer)
+        const fileWithoutExt = afbeelding.filename.split('.').at(-2).split('/').at(-1)
+
+        if (fileType.ext) {
+          const outputFileName = `../../maljaars/public/images/${fileWithoutExt}.${fileType.ext}`
+          console.log(`write ${outputFileName}`)
+          fs.createWriteStream(outputFileName).write(buffer);
+        } else {
+          console.log('File type could not be reliably determined! The binary data may be malformed! No file saved!')
+        }
+      }
+
+      download()
+
+      afbeeldingen.push({
+        name: filename,
+        path: filename,
+      })
+    });
+
     return {
+      nummer: index + 1,
       name: story.name,
-      slug: story.slug,
-      ...story.content
+      omschrijving: Storyblok.richTextResolver.render(story.content.omschrijving),
+      afbeeldingen: afbeeldingen
     }
   })
 
-  const csv = json2csv.parse(mapped, { fields: Object.keys(mapped[0]) })
-  
-  fs.writeFileSync('./export/' + Date.now() + '.csv', csv)
+  fs.writeFileSync('../../maljaars/public/kavels.json', JSON.stringify(mapped, null, 2))
 }
 
 start()
